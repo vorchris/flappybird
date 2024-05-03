c3d711ca5664467bde519925586303774e277473
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-03 12:10:34 +0200

    add gravity

--- src/main/java/at/flappybird/game/Data.java
+++ src/main/java/at/flappybird/game/Data.java
@@ -1,9 +1,12 @@
 package at.flappybird.game;
 
 public class Data {
-    public static class Images{
-        public static final String pipeTop = "assets/Images/pipe-top.png";
-        public static final String pipeBottom = "assets/Images/pipe-bottom.png";
-        public static final String birdBody = "assets/Images/pipe-body.png";
-    }
+  public static class Images {
+    public static final String pipeTop = "assets/Images/pipe-top.png";
+    public static final String pipeBottom = "assets/Images/pipe-bottom.png";
+    public static final String birdBody = "assets/Images/pipe-body.png";
+  }
+  public static class Settings {
+    public static final int gravity = 30;
+  }
 }
