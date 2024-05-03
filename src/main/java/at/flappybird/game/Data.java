702c8a362ceaac0103fa149185c1b8d7d66845a0
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-03 12:14:27 +0200

    fix paths

--- src/main/java/at/flappybird/game/Data.java
+++ src/main/java/at/flappybird/game/Data.java
@@ -3,8 +3,8 @@ package at.flappybird.game;
 public class Data {
   public static class Images {
     public static final String pipeTop = "assets/Images/pipe-top.png";
-    public static final String pipeBottom = "assets/Images/pipe-bottom.png";
-    public static final String birdBody = "assets/Images/pipe-body.png";
+    public static final String pipeBody = "assets/Images/pipe-body.png";
+    public static final String bird = "assets/Images/player.png";
   }
   public static class Settings {
     public static final int gravity = 30;
