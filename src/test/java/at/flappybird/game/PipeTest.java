26d192f2c7ff019aa07255b80068504f01022f73
vorchris <christian.vor26@gmail.com>
2024-05-24 10:38:25 +0200

    added Test Class

---
+++ src/test/java/at/flappybird/game/PipeTest.java
@@ -0,0 +1,28 @@
+package at.flappybird.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PipeTest {
+
+  @Test
+  void setX() {
+  }
+
+  @Test
+  void colliding() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void getX() {
+  }
+
+  @Test
+  void getHeight() {
+  }
+}
