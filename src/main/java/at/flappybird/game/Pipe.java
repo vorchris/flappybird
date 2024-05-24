0efe2a03999c4eba5ef9cd65653c8dfd4188f9ee
vorchris <christian.vor26@gmail.com>
2024-05-24 10:36:50 +0200

    made code prettyer

--- src/main/java/at/flappybird/game/Pipe.java
+++ src/main/java/at/flappybird/game/Pipe.java
@@ -1,79 +1,70 @@
 package at.flappybird.game;
-
-import java.util.ArrayList;
-import java.util.Random;
 import javafx.geometry.Bounds;
 import javafx.scene.Group;
 import javafx.scene.image.ImageView;
 import lombok.Getter;
-import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Pipe extends Group {
-    @Getter int x;
-    int amount;
-    int height;
+  @Getter int x, height;
+  ArrayList<ImageView> allImages = new ArrayList<>();
 
-    ArrayList<ImageView> allImages = new ArrayList<>();
-    public Pipe(int height) {
-        this.amount = height / 32 + 1;
-        this.height = height;
+  public Pipe(int height) {
+    int amount = calculateAmount(height);
+    this.height = height;
+    int opening = getRandomGap(amount);
 
-        int opening = getRandomGap();
-        for (int i = 0; i < amount; i++) {
-            if (i != opening && i != opening + 1) {
-                ImageView iw = new ImageView(Data.Images.pipeBody);
-                allImages.add(iw);
-                iw.setFitHeight(32);
-                iw.setFitWidth(32);
-                iw.setX(0);
-                iw.setY(i * 32);
-                this.getChildren().add(iw);
-            }
-        }
-        ImageView top = new ImageView(Data.Images.pipeTop);
-        ImageView bottom = new ImageView(Data.Images.pipeTop);
+    IntStream.range(0, amount).forEach(i -> {
+      if (!isOpening(i, opening)) {
+        ImageView iw = createImageView(Data.Images.pipeBody, i);
+        this.getChildren().add(iw);
+        allImages.add(iw);
+      }
+    });
 
-        allImages.add(top);
-        allImages.add(bottom);
+    ImageView top = createImageView(Data.Images.pipeTop, opening - 1);
+    top.setScaleY(-1);
+    ImageView bottom = createImageView(Data.Images.pipeTop, opening + 2);
+    this.getChildren().addAll(top, bottom);
+    allImages.add(top);
+    allImages.add(bottom);
+  }
 
-        top.setX(0);
-        bottom.setX(0);
+  private int calculateAmount(int height) {
+    return IntStream.iterate(height, h -> h - 32).takeWhile(h -> h > 0).map(h -> 1).sum();
+  }
 
-        top.setFitHeight(32);
-        top.setFitWidth(32);
-        top.setScaleY(-1);
+  private boolean isOpening(int index, int opening) {
+    return index == opening || index == opening + 1;
+  }
 
-        bottom.setFitHeight(32);
-        bottom.setFitWidth(32);
+  private ImageView createImageView(String imagePath, int position) {
+    ImageView iw = new ImageView(imagePath);
+    iw.setFitHeight(32);
+    iw.setFitWidth(32);
+    iw.setX(0);
+    iw.setY(position * 32);
+    return iw;
+  }
 
-        top.setY((opening - 1) * 32);
-        bottom.setY((opening + 2) * 32);
-        this.getChildren().addAll(top, bottom);
-    }
-    private int getRandomGap() {
-        Random rand = new Random();
-        System.out.println("random number form 3 to " + (amount - 1));
-        return rand.nextInt(3, amount - 1);
-    }
-    public void setX(int x) {
-        this.x = x;
-        allImages.forEach(a -> a.setX(x));
-    }
+  private int getRandomGap(int amount){
+    return new Random().ints(3, amount - 1).findFirst().getAsInt();
+  }
 
-    public boolean colliding(ImageView iw) {
-        Bounds b1 = iw.getBoundsInParent();
-        if (b1.getMaxY() >= height || b1.getMinY() <= 0) {
-            return true;
-        }
-        for (ImageView imageView : allImages) {
-            Bounds b2 = imageView.getBoundsInParent();
-            if (b1.intersects(b2)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    public void delete() {
-        allImages.forEach(a -> this.getChildren().remove(a));
-    }
+  public void setX(int x){
+    this.x = x;
+    allImages.forEach(a -> a.setX(x));
+  }
+
+  public boolean colliding(ImageView iw){
+    Bounds b1 = iw.getBoundsInParent();
+    return b1.getMaxY() >= height || b1.getMinY() <= 0 || allImages.stream().anyMatch(img -> b1.intersects(img.getBoundsInParent()));
+  }
+
+  public void delete(){
+    allImages.forEach(a -> getChildren().remove(a));
+  }
 }
