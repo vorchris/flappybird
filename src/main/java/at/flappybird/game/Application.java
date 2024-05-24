a2c63a383eeebf0b8520447d83313846a55f870d
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-24 12:00:26 +0200

    structure fix

--- src/main/java/at/flappybird/game/Application.java
+++ src/main/java/at/flappybird/game/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader =
-            new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+            new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
         stage.setTitle("Flappybird");
         stage.setScene(scene);
