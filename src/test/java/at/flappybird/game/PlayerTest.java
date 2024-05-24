bda7cd9632cb963757884c862352fc9eac502548
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-24 13:42:55 +0200

    player test now works

--- src/test/java/at/flappybird/game/PlayerTest.java
+++ src/test/java/at/flappybird/game/PlayerTest.java
@@ -1,17 +1,25 @@
 package at.flappybird.game;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
 
-public class PlayerTest {
+public class PlayerTest extends ApplicationTest {
 
-    private Player player = new Player();
+    private Player player;
+
+    @Override
+    public void start(Stage stage) {
+        player = new Player();
+    }
 
     @Test
-    void testRestart() {
+    void restart() {
         player.restart();
-        System.out.println("test");
+        assertEquals(2, player.getSpeed());
         assertEquals(100, player.getImageView().getY());
     }
 }
