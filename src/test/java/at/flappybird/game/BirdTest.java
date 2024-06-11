6ba3048f665e721457d4406fbbd363bd7de0f687
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-06-11 15:32:08 +0200

    fix tests

---
+++ src/test/java/at/flappybird/game/BirdTest.java
@@ -0,0 +1,13 @@
+package at.flappybird.game;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.Instant;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class BirdTest extends ApplicationTest {
+    private Bird bird;
+}
