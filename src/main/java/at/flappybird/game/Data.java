45528c78d30aa50aea9006b3ab3c252d5c98138c
raphael <rladinig@tsn.at>
2024-06-11 12:42:41 +0100

    sounds gehen und gradle auch war nur linux issue habe gradle gerade auf win11 getestet und auf gentoo also kp was bei dir nit geht chris

--- src/main/java/at/flappybird/game/Data.java
+++ src/main/java/at/flappybird/game/Data.java
@@ -5,6 +5,8 @@ public class Data {
         public static final String pipeTop = "file:assets/Images/pipe-top.png";
         public static final String pipeBody =
             "file:assets/Images/pipe-body.png";
+        public static final String background = "file:assets/Images/background.png";
+        public static final String base = "file:assets/Images/base.png";
         public static final String bird = "file:assets/bird.png";
     }
     public static class Settings {
@@ -17,5 +19,8 @@ public class Data {
     }
     public static class Sounds {
         public static final String flap = "assets/flap.wav";
+        public static final String die = "assets/die.wav";
+        public static final String themeSong = "assets/theme-song.wav";
+        public static final String point = "assets/point.wav";
     }
 }
